e5978542075cdcd454a0fbc975d8909128d7a280
avitaind <avitaind@gmail.com>
2022-02-24 15:19:16 +0530

    home page completed

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Sidebar from './layouts/Sidebar';
+import HomeBg from '../img/home-bg.jpg';
+import { Link } from 'react-router-dom';
+
+
+const styles = {
+  homeBackground: {
+      backgroundImage: `url(${HomeBg})`,
+  }
+};
+
 
 export default function Home() {
   return (
     <div className='row'>
-      <div className='col-md-4'>
+      <div className='col-md-2'>
         <Sidebar />
       </div>
-      <div className='col-md-8'>
-        Home
+      <div className='card home-card col-md-10' style={styles.homeBackground}>
+      <div className="sub-panel text-center mt-5 py-5 px-5">
+          <h2 className="my-4 ls-5">Become an AVITA Member</h2>
+          <div className="py-0 sub-panel-content">
+              <div className="home-text my-4 mx-2 ls-1">In addition to AVITA intermittently offering the hottest discounts, news on sales promotions, <br/>and the newest information on AVITA, you will also enjoy having excellent technological support services to promote your experience as a user of our products.</div>
+          </div>
+
+          <Link className="btn btn-register my-5 px-4 ls-2" to="/warranty-registration">Register Your Device</Link>
+      </div>
+
+
       </div>
 
     </div>
